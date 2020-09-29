e2a88e5262813bdacea00cad25745a0f58f50c31
Connor <melty100@gmail.com>
2020-09-28 17:03:33 -0700

    initializing repo

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS `burgers_db`;
+CREATE DATABASE `burgers_db`;
+
+USE `burgers_db`;
+
+DROP TABLE IF EXISTS `burgers`;
+CREATE TABLE `burgers` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`burger_name` VARCHAR(40) NOT NULL,
+    `devoured` BOOLEAN NOT NULL,
+    PRIMARY KEY (`id`)
+);
---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Chicken_Sandwhich', false),
+       ('Southwest_Burger', false),
+       ('Turkey_Burger', false);
